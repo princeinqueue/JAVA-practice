ff9522d0371f44dd68f2e47f9526747d6d1d943e
princeinqueue <princemangla43@gmail.com>
2026-07-22 02:31:38 +0530

    Add files via upload

---
+++ verifyprime.java
@@ -0,0 +1,12 @@
+static boolean verifyPrime(int n) {
+  boolean prime=true;
+    if(n==0 || n==1){
+        return false;
+    }
+    for(int i=2;i<=Math.sqrt(n);i++){
+        if(n%i==0){
+            prime=false;
+        }
+    }
+    return prime;
+}
